f2a3b507d2a70c44b69f6913e0dfb4b2d59ad75c
Enes Baysal <enesbaysal_68@hotmail.com>
2024-09-16 23:33:20 +0200

    change role to style

--- src/main/java/com/furiousheroes/model/User.java
+++ src/main/java/com/furiousheroes/model/User.java
@@ -18,7 +18,7 @@ public class User {
     private String userName;
     private String password;
     private String email;
-    private String role;
+    private String style;
     private long experience;
     private long gold;
     private long wood;
